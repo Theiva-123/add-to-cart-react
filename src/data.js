5b09412bf1c9cc227da8fda0cef9497833646575
Theiva-123 <theivadhoni@gmail.com>
2023-05-25 00:01:29 -0700

    add to cart

---
+++ src/data.js
@@ -0,0 +1,35 @@
+import img1 from './imges/electronic.webp';
+import img2 from './imges/phone.webp';
+import img3 from './imges/pic 1.webp';
+
+
+const data = {
+    productData:[
+    {
+      id: 1,
+      img: img1,
+      title:'Tv Watches',
+      desc:'',
+      price:19999,
+    },
+
+    {
+        id: 2,
+        img: img2,
+        title:'phones',
+        desc:'',
+        price:39999,
+      },
+
+      {
+        id: 3,
+        img: img3,
+        title:'shoes',
+        desc:'',
+        price:1999,
+      },
+
+    ],
+};
+
+export default data;
